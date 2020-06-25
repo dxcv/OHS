acc797a02606f7b469d8b04ec35de7423bab8109
Hanyuan Hu <hoorex@outlook.com>
2020-06-25 20:40:59 +0800

    take arbitrary vol position by bars

--- hedging_strategies.py
+++ hedging_strategies.py
@@ -3,9 +3,8 @@ from numpy import ndarray as np_ndarray
 from numpy import array as np_array
 from numpy import random as np_rd
 from numpy import isnan as np_isnan
-from numpy import ones as np_ones
 from numpy import zeros as np_zeros
-from scipy.optimize import least_squares
+from scipy.optimize import minimize
 from multiprocessing import Pool
 from functools import partial
 from typing import Dict, Tuple, List
@@ -24,39 +23,43 @@ def gen_risk_metrics(p_iv_dct: Dict[str, pd_DataFrame], u_p_df) -> Tuple[np_ndar
            list(p_iv_dct.keys()) + u_p_df.columns.to_list()
 
 
-def delta_neutral_ct(port_mat: np_ndarray) -> float:
-    return port_mat[[1, 2, 5]]  # delta, gamma, vega
+def hedging_criteria(port_mat: np_ndarray) -> float:
+    # ['d_p', 'delta', 'gamma', 'rho', 'theta', 'vega']
+    return port_mat[1]**2
 
 
-def optimize_one_day(port_mat: np_ndarray, op_ct_func, hg_tools=(-1,)) -> np_ndarray:
+def optimize_one_day(i: int, op_ct_func, pw_mat: np_ndarray, o_cub: np_ndarray, hg_tools: Tuple = (-1,)) -> np_ndarray:
+    print(i)
+    port_mat = o_cub[:, i, :]
+    pw_ar = pw_mat[:, i, :]
     hg_tools = list(hg_tools)
+    pw_ar = pw_ar.reshape(-1)
 
     def _op_tgt(w):
-        tw = np_ones(w.shape, dtype=float)
-        tw[hg_tools] = w[hg_tools]
+        tw = pw_ar.copy()
+        tw[hg_tools] = w
         return op_ct_func(port_mat.T @ tw)
 
-    w_0 = np_ones(port_mat.shape[0], dtype=float)
-    w_0[hg_tools] = np_rd.randn(len(hg_tools))
+    w_0 = np_rd.randn(len(hg_tools))
 
     tr = np_zeros(port_mat.shape[0], dtype=float)
-    tr[hg_tools] = least_squares(_op_tgt, w_0).x[hg_tools]
+    tr[hg_tools] = minimize(_op_tgt, w_0, method="nelder-mead").x
     return tr
 
 
-def calc_hedge_w_n_expo(op_iv_dct: Dict[str, pd_DataFrame], u_p_df: pd_DataFrame, pw_ar: np_ndarray, hg_tools=(-1,)) \
-        -> Tuple[pd_DataFrame, pd_DataFrame]:
+def calc_hedge_w_n_expo(op_iv_dct: Dict[str, pd_DataFrame], u_p_df: pd_DataFrame, pw_mat: np_ndarray,
+                        hg_tools: Tuple = (-1,)) -> Tuple[pd_DataFrame, pd_DataFrame]:
     o_cub, cols = gen_risk_metrics(op_iv_dct, u_p_df)
     o_cub = o_cub.astype(float)
     o_cub[np_isnan(o_cub)] = 0.0
 
     pool = Pool()
-    hw_mat = np_array(pool.map(partial(optimize_one_day, op_ct_func=delta_neutral_ct, hg_tools=hg_tools),
-                     [o_cub[:, i, :] for i in range(o_cub.shape[1])]))
+    hw_mat = np_array(pool.map(partial(optimize_one_day, op_ct_func=hedging_criteria,
+                                       hg_tools=hg_tools, pw_mat=pw_mat, o_cub=o_cub), list(range(o_cub.shape[1]))))
     pool.close()
     pool.join()
 
-    hg_rst = (o_cub.swapaxes(0, 1) * (pw_ar[:, :, 0] + hw_mat.reshape(*hw_mat.shape, 1))).sum(axis=1)
+    hg_rst = (o_cub.swapaxes(0, 1) * (pw_mat.swapaxes(0, 1) + hw_mat.reshape(*hw_mat.shape, 1))).sum(axis=1)
 
     return pd_DataFrame(data=hw_mat, columns=cols, index=u_p_df.index), \
            pd_DataFrame(data=hg_rst, columns=PF_DF_COLS, index=u_p_df.index)
@@ -69,4 +72,3 @@ def calc_portfolio_pnl(w_df: pd_DataFrame, d_op_df: pd_DataFrame, u_op_df: pd_Da
     u_rt_df = u_op_df.diff(1).shift(-1)
     return (pw_ar[:-1, 0, 0] * d_rt_df).join(pw_ar[-1, 0, 0] * u_rt_df).sum(1), \
            (w_df.iloc[:, :-1] * d_rt_df).join(w_df.iloc[:, [-1]] * u_rt_df).sum(1)
-
--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 """
-1. load option list, together with underlying and maturity date
-2. fetch price data from WindPy if not exist in cache
-3. calculate Greeks using some Pricing Model(PM)
-4. simulate hedge using some hedging strategy(HS), calculate running heding PnL
-5. present and report
+two usage:
+1. exotic hedging strategy search
+2. volatility portfolio management
 """
 
 from data_preprocsssing import *
@@ -21,9 +19,10 @@ if __name__ == '__main__':
 
     op_iv_dct = gen_p_iv_gks_dct(d_df["d_code"], d_t_df, u_op_df, d_tkr2info)
 
-    pw_ar = -1 * np.ones((d_df.shape[0] + 1, 1, 1), dtype=float)
-    pw_ar[-1, 0, 0] = 0.0
-    w_df, hr_df = calc_hedge_w_n_expo(op_iv_dct, u_op_df, pw_ar, range(87))
-    pw_pnl_df, hg_pnl_df = calc_portfolio_pnl(w_df, d_op_df, u_op_df, pw_ar)
+    # pw_mat: (inst count + 1, bars, 1)
+    pw_mat = -1 * np.ones((d_op_df.shape[1] + 1, d_op_df.shape[0], 1), dtype=float)
+    pw_mat[-1, :, 0] = 0.0
+    w_df, hr_df = calc_hedge_w_n_expo(op_iv_dct, u_op_df, pw_mat, range(87))
+    pw_pnl_df, hg_pnl_df = calc_portfolio_pnl(w_df, d_op_df, u_op_df, pw_mat)
 
     print(1)
